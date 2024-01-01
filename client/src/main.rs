8598dd6eb4124662153bbbd9fbebbd90b46644be
Lingkang <contact@lingkang.dev>
2024-01-01 22:06:05 +0000

    [client] generate random tasks for server

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,9 +2,10 @@ mod protocol;
 
 use std::{
     io::{Read, Write},
-    net::TcpStream,
-    vec,
+    net::{Shutdown, TcpStream},
 };
+
+use rand::Rng;
 
 use crate::protocol::{
     message::{Byte, Message},
@@ -12,6 +13,7 @@ use crate::protocol::{
 };
 
 const URL: &str = "test.lingkang.dev:8333";
+const MAX_TASKS: usize = 4;
 
 fn main() {
     let mut stream = match TcpStream::connect(URL) {
@@ -23,15 +25,20 @@ fn main() {
     };
     receive_message(&mut stream);
 
-    let msg = Message::new_request(vec![0x00, 0x02]);
-    let msg_str: Vec<Byte> = serialize(&msg);
-    print!("Sending (in decimal): {:?}\n", msg_str);
-    let _ = stream.write_all(&msg_str);
-    receive_message(&mut stream);
+    for _ in 0..MAX_TASKS {
+        let msg = Message::new_request(collect_a_task());
+        let msg_str: Vec<Byte> = serialize(&msg);
+        print!("Sending (in decimal): {:?}\n", msg_str);
+        stream.write_all(&msg_str).unwrap();
+        receive_message(&mut stream);
+    }
+
+    stream.shutdown(Shutdown::Both).unwrap();
 
     println!("Exiting...");
 }
 
+/// Receives a message from the server and prints it.
 fn receive_message(stream: &mut TcpStream) {
     let mut buffer: [Byte; 64] = [0; 64];
     let bytes_read = stream.read(&mut buffer).unwrap();
@@ -39,4 +46,28 @@ fn receive_message(stream: &mut TcpStream) {
         "Received: {}",
         String::from_utf8_lossy(&buffer[..bytes_read])
     );
+}
+
+/// Generates a random [`f64`] number.
+fn get_random_f64() -> f64 {
+    let mut rng = rand::thread_rng();
+    rng.gen::<f64>() * 100.0
+}
+
+/// Generates a pair of random [`f64`] numbers,
+/// which will be used as the sides of a right triangle.
+fn get_sides() -> (f64, f64) {
+    (get_random_f64(), get_random_f64())
+}
+
+/// Basically generates a task of calculating the
+/// hypotenuse of a right triangle,
+/// and returns the sides of the triangle as a [`Vec`] of [`Byte`]s.
+fn collect_a_task() -> Vec<Byte> {
+    let (a, b) = get_sides();
+    println!("a = {}, b = {}", a, b);
+    let mut sides: Vec<Byte> = Vec::new();
+    sides.extend(a.to_be_bytes().to_vec());
+    sides.extend(b.to_be_bytes().to_vec());
+    sides
 }
